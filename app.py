df88f38257841a6b8c2685e55b1e033fa75d9114
nicolasromano <naroman2@asu.edu>
2022-03-12 20:04:29 -0500

    fix

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import plotly.express as px
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 from skimage import data
 
 img = data.chelsea()
