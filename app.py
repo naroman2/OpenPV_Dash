aec8ba4083e08c9e1d57c837e7d7261e739cee98
Nicolas Romano <43258647+naroman2@users.noreply.github.com>
2022-03-26 14:07:33 -0400

    Original Commit

---
+++ app.py
@@ -0,0 +1,136 @@
+# This file is the main plotly app file with web-browser components and callback actions
+
+# Plotly Dash Imports:
+import plotly.express as px
+import dash
+from dash import dcc, html, State
+from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
+
+# image processing imports:
+from PIL import Image
+from skimage import data, io
+
+# internet requests imports:
+import requests
+import urllib.request
+
+# other imports:
+import os
+import math
+import xmltodict
+from satellite_img import get_img_file
+
+
+# Nicolas's API Key for Bing Maps:
+BingMapsKey = 'AgPVfqkpC5Z-qfxWFx2tOOgFwsr-hMNKrZC5gX5D7EqVnkHRSbZQfRf0eoYxQsgz'
+
+# Black Image for initial website (Could be replaced with a OpenPV logo?)
+# Need to figure out how to make rectangle a brighter color like yellow?
+img = io.imread('black.jpg')
+fig = px.imshow(img)  # creating a plotly graph figure
+fig.update_layout(dragmode="drawrect")  # enabling rectangle drawing upon drags
+
+# Configuration dictionary for the graph interactions enabled:
+config = {
+    "modeBarButtonsToAdd": [
+        #"drawline",
+        #"drawopenpath",
+        #"drawclosedpath",
+        #"drawcircle",
+        "drawrect",
+        "eraseshape",
+    ],
+    "fillFrame": True
+}
+
+# creating the dash app and server
+app = dash.Dash('OpenPV')
+server = app.server
+
+# Website layout which contains HTML and Dash Components
+app.layout = html.Div(
+        [
+        # Header Title:
+        html.H3('OpenPV - Photovoltaic System Prediction Tool'), 
+
+        # Input box for address:
+        dcc.Input(id = 'address_input', type = 'text', placeholder = 'type your address here', size = '100'),
+
+        # Submit Button to click after entering in user's address
+        html.Button('Submit', id='submit-val', n_clicks=0), 
+
+        # Text Area to display longitude and latitude which is found via Bing Maps API
+        html.Div(id='gps_coords', children='GPS Coordinates'),
+
+        
+        html.Button('Get Rooftop Image', id='satellite-btn', n_clicks=0),
+        html.Div(id = 'graph_div', children = [dcc.Graph(id = 'graph')], style={"display": "inline-block"}),
+        html.Div(id = 'zoom_graph_div', children = [dcc.Graph(id = 'zoom-graph')], style={"display": "inline-block"})
+        ]
+)
+
+
+# Callback for the Submit Button
+@app.callback(
+    # update the output of the 
+    Output('gps_coords', 'children'),
+    Input('submit-val', 'n_clicks'),
+    State('address_input', 'value')
+)
+def get_lat_lng(n_clicks, address):      
+    try: 
+        street_address = '%20'.join(address.split())
+        bing_maps_url = f'http://dev.virtualearth.net/REST/v1/Locations?addressLine={street_address}&key={BingMapsKey}&o=xml'
+        r = requests.get(bing_maps_url)
+        obj = xmltodict.parse(r.text)
+        coords = dict(obj.get('Response').get('ResourceSets').get('ResourceSet').get('Resources').get('Location').get('Point'))
+        lat = coords.get('Latitude')
+        lng = coords.get('Longitude')
+        coords = f'{lat}, {lng}'
+        return coords
+    except:
+        return ''
+@app.callback(
+    Output('graph_div', 'children'),
+    Input('satellite-btn', 'n_clicks'),
+    State('gps_coords', 'children')
+)
+def get_rooftop_img(n_clicks, lat_lng):
+    try:
+        lat = lat_lng.split(',')[0]
+        lng = lat_lng.split(',')[1]
+        global satellite_img 
+        satellite_img = get_img_file(float(lat), float(lng))
+        fig = px.imshow(satellite_img)
+        fig.update_layout(dragmode="drawrect", width=800, height=800, autosize=False,)
+        return dcc.Graph(id = 'graph', figure = fig, config = config)
+    except:
+        img = io.imread('black.jpg')
+        fig = px.imshow(img)
+        fig.update_layout(dragmode="drawrect", width=800, height=800, autosize=False,)
+        return dcc.Graph(id = 'graph', figure = fig, config = config)
+
+@app.callback(
+    Output("zoom_graph_div", "children"),
+    Input("graph", "relayoutData"),
+    prevent_initial_call=True,
+)
+def on_new_annotation(relayout_data):
+    try:
+        if "shapes" in relayout_data:
+            last_shape = relayout_data["shapes"][-1]
+            # shape coordinates are floats, we need to convert to ints for slicing
+            x0, y0 = int(last_shape["x0"]), int(last_shape["y0"])
+            x1, y1 = int(last_shape["x1"]), int(last_shape["y1"])
+            roi_img = satellite_img[y0:y1, x0:x1]
+            fig = px.imshow(roi_img)
+            return dcc.Graph(id = 'zoom-graph', figure = fig)
+    except:
+        img = io.imread('black.jpg')
+        fig = px.imshow(img)
+        fig.update_layout(dragmode="drawrect", width=800, height=800, autosize=False,)
+        return dcc.Graph(id = 'zoom-graph', figure = fig, config = config)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
---
+++ satellite_img.py
@@ -0,0 +1,64 @@
+import os
+from PIL import Image
+import math
+import urllib.request
+import numpy as np
+
+class GoogleMapsLayers:
+  ROADMAP = "v"
+  TERRAIN = "p"
+  ALTERED_ROADMAP = "r"
+  SATELLITE = "s"
+  TERRAIN_ONLY = "t"
+  HYBRID = "y"
+def get_img_file(lat, lng):
+    zoom = 19
+    layer = GoogleMapsLayers.SATELLITE
+
+    # tiles
+    tile_size = 256
+    num_tiles = 1 << zoom    # => 2^zoom number of tiles
+    point_x = int((tile_size / 2 + lng * tile_size / 360.0) * num_tiles // tile_size)
+    sin_y = math.sin(lat * (math.pi / 180.0))
+    # get y equivalent of the latitude
+    point_y = int(((tile_size / 2) + 0.5 * math.log((1 + sin_y) / (1 - sin_y)) * -(tile_size / (2 * math.pi))) * num_tiles // tile_size)
+    # start x and y for the top left tile coordinates
+    start_x = point_x
+    start_y = point_y
+
+    # sets the number of tiles composing the image:
+    tile_width = 3
+    tile_height = 3
+
+    # getting the pixel width and height for the entire tile composed image
+    width = tile_size * tile_width
+    height = tile_size * tile_width
+
+    # creating a new blank image with the desired pixel dimensions.  will be used to store the final image
+    map_img = Image.new('RGB', (width, height))
+    offset_x = -1
+    offset_y = 0
+    start_x = start_x + offset_x
+    start_y = start_y + offset_y
+    
+    for x in range(0, tile_width):
+        for y in range(0, tile_height):
+
+            # creating google image request url:
+            url = f'https://mt1.google.com/vt/lyrs={layer}&x={start_x + x}&y={start_y + y}&z={zoom}'
+
+            # tile image file name:
+            current_tile = f'{str(x)}-{str(y)}'
+
+            # the tile image data is store into current_tile after url request
+            urllib.request.urlretrieve(url, current_tile)
+
+            # open that newly received image tile
+            im = Image.open(current_tile)
+
+            # paste that tile in its correct slot
+            map_img.paste(im, (x * 256, y * 256))
+            os.remove(current_tile)
+    
+    return np.array(map_img)
+
